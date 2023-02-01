3b2e6515c13e981bd35c26e1eaf03aa378195ad8
syalicheff <yalicheff.sebastien@gmail.com>
2023-02-02 00:29:13 +0100

    Routes à jour

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -14,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/category')]
 class CategoryController extends AbstractController
 {
-    #[Route('/', name: 'app_category_index', methods: ['GET'])]
+    #[Route('/', name: 'category_index', methods: ['GET'])]
     public function index(CategoryRepository $categoryRepository): Response
     {
         return $this->render('front/category/index.html.twig', [
@@ -22,27 +22,7 @@ class CategoryController extends AbstractController
         ]);
     }
 
-
-    #[Route('/new', name: 'app_category_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, CategoryRepository $categoryRepository): Response
-    {
-        $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $categoryRepository->save($category, true);
-
-            return $this->redirectToRoute('front_app_category_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('front/category/new.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_category_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'category_show', methods: ['GET'])]
     public function show(Category $category, ArticleRepository $articleRepository): Response
     {
         $articles = $articleRepository->findByCategory($category);
@@ -51,33 +31,5 @@ class CategoryController extends AbstractController
             'category' => $category,
             'articles' => $articles,
         ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_category_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Category $category, CategoryRepository $categoryRepository): Response
-    {
-        $form = $this->createForm(CategoryType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $categoryRepository->save($category, true);
-
-            return $this->redirectToRoute('front_app_category_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('front/category/edit.html.twig', [
-            'category' => $category,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_category_delete', methods: ['POST'])]
-    public function delete(Request $request, Category $category, CategoryRepository $categoryRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
-            $categoryRepository->remove($category, true);
-        }
-
-        return $this->redirectToRoute('front_app_category_index', [], Response::HTTP_SEE_OTHER);
     }
 }
