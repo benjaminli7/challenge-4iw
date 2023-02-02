1175bd78b92af2d0e58f0f903488a1f53ec47f9e
benjaminli7 <nicky01.bl@gmail.com>
2023-02-02 20:22:53 +0100

    add integration login view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'primary-red': '#76004F',
+      'primary-gray': '#F5F5F5',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      yellow: colors.yellow,
+      blue: colors.blue,
+      red: colors.red,
+    },
+    extend: {
+      backgroundImage: {
+        'login': "url('/public/images/bg-login.png')",
+      },
+    },
+    fontFamily: {
+      'raleway': ['Raleway', 'sans-serif'],
+    }
   },
   plugins: [],
 }
