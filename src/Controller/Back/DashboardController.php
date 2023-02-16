d7fc4c6258d46edb7ba13a9f0f4c6d60adce74ac
benjaminli7 <nicky01.bl@gmail.com>
2023-02-16 02:33:35 +0100

    add item menu, add employees files

--- src/Controller/Back/DashboardController.php
+++ src/Controller/Back/DashboardController.php
@@ -7,13 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/')]
-class DefaultController extends AbstractController
+class DashboardController extends AbstractController
 {
     #[Route('', name: 'default_index')]
     public function index(): Response
     {
-        return $this->render('back/default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->render('back/dashboard/index.html.twig');
     }
 }
