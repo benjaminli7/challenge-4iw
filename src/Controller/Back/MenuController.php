513da0a5034db37299444a1bdedb8c3dc3232e02
benjaminli7 <nicky01.bl@gmail.com>
2023-02-18 04:22:03 +0100

    add entity order, add fixtures, edit admin menu view

---
+++ src/Controller/Back/MenuController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Back;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\ArticleRepository;
+
+
+
+#[Route('/menu')]
+class MenuController extends AbstractController
+{
+    #[Route('/', name: 'app_menu')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('back/menu/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+}
