870b749ba62d2ee7002de74395853576148a28af
benjaminli7 <nicky01.bl@gmail.com>
2023-02-18 22:46:08 +0100

    add cart controller

--- src/Controller/Front/CartController.php
+++ src/Controller/Front/CartController.php
@@ -2,24 +2,16 @@
 
 namespace App\Controller\Front;
 
-use App\Entity\Article;
-use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 #[Route('/cart')]
 class CartController extends AbstractController
 {
-    #[Route('/', name: 'cart_index', methods: ['GET'])]
-    public function index(ArticleRepository $articleRepository): Response
+    #[Route('/', name: 'app_cart')]
+    public function index(): Response
     {
-        $articles = $articleRepository->findAll();
-
-        return $this->render('front/article/index.html.twig', [
-            'articles' => $articles,
-        ]);
+        return $this->render('front/cart/index.html.twig');
     }
-
 }
