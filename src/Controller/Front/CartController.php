db6813a03b419549eba7b49843025fea2a68dc71
benjaminli7 <nicky01.bl@gmail.com>
2023-02-20 20:30:30 +0100

    add integration stripe payment with cart

--- src/Controller/Front/CartController.php
+++ src/Controller/Front/CartController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
 
 #[Route('/cart')]
 class CartController extends AbstractController
@@ -64,6 +66,43 @@ class CartController extends AbstractController
         ]);
     }
 
+    #[Route('/checkout', name: 'checkout')]
+    public function checkout(Request $request): Response
+    {
+        $cart = $request->getSession()->get('cart', []);
+
+        $baseUrl = $request->getSchemeAndHttpHost();
+
+        Stripe::setApiKey($this->getParameter('STRIPE_SECRET'));
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => $this->getLineItems($cart),
+            'mode' => 'payment',
+            'success_url' => $baseUrl . $this->generateUrl('client_checkout_success'),
+            'cancel_url' => $baseUrl . $this->generateUrl('client_checkout_cancel'),
+        ]);
+
+        return $this->redirect($session->url);
+    }
+
+
+
+    #[Route('/checkout/success', name: 'checkout_success')]
+    public function checkoutSuccess(Request $request): Response
+    {
+
+        $request->getSession()->remove('cart');
+
+        return $this->render('front/cart/checkout_success.html.twig');
+    }
+
+    #[Route('/checkout/cancel', name: 'checkout_cancel')]
+    public function checkoutCancel(): Response
+    {
+        return $this->render('front/cart/checkout_cancel.html.twig');
+    }
+
     private function getCartItemCount(Request $request): int
     {
         $cart = $request->getSession()->get('cart', []);
@@ -77,6 +116,27 @@ class CartController extends AbstractController
         return $count;
     }
 
+    
+    private function getLineItems(array $cart): array
+    {
+        $lineItems = [];
+
+        foreach ($cart as $cartItemData) {
+            $article = $cartItemData['article'];
+            $lineItems[] = [
+                'price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => (int) ($article->getPrice() * 100),
+                    'product_data' => [
+                        'name' => $article->getName(),
+                    ],
+                ],
+                'quantity' => $cartItemData['quantity'],
+            ];
+        }
+
+        return $lineItems;
+    }
 
 
 
