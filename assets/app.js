c33da297089d19bcaa131e9b55a5472c49685a43
benjaminli7 <nicky01.bl@gmail.com>
2023-02-20 18:02:06 +0100

    add cart template, need to add stripe checkout

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,3 @@
 import './styles/app.scss';
 import 'flowbite';
 
-// client
-window.addEventListener('load', function() {
-  updateCartCount();
-
-    const addButtons = document.querySelectorAll('.add-to-cart');
-    addButtons.forEach(button => {
-      button.addEventListener('click', function() {
-        const id = button.dataset.id;
-        const name = button.dataset.name;
-        const price = button.dataset.price;
-        addToCart(name, price);
-        updateCartCount();
-      });
-    });
-  });
-  
-  function addToCart(name, price) {
-    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-    cartItems.push({ name: name, price: price });
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  }
-  
-  function updateCartCount() {
-    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
-    const cartCount = document.querySelector('.cart-count');
-    cartCount.textContent = cartItems.length;
-  }
-  
--- src/Controller/Front/CartController.php
+++ src/Controller/Front/CartController.php
@@ -5,13 +5,80 @@ namespace App\Controller\Front;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 #[Route('/cart')]
 class CartController extends AbstractController
 {
     #[Route('/', name: 'app_cart')]
-    public function index(): Response
+    public function index(Request $request, ArticleRepository $articleRepository): Response
     {
-        return $this->render('front/cart/index.html.twig');
+        $cart = $request->getSession()->get('cart', []);
+        $priceTotal = 0;
+
+        $cartArticles = [];
+    
+        foreach ($cart as $articleId => $quantity) {
+            $article = $articleRepository->find($articleId);
+    
+            if ($article) {
+                $priceTotal += $article->getPrice() * $quantity['quantity'];
+                $cartArticles[] = [
+                    'article' => $article,
+                    'quantity' => $quantity['quantity'],
+                ];
+            }
+        }
+
+        return $this->render('front/cart/index.html.twig', [
+            'cartArticles' => $cartArticles,
+            'priceTotal' => $priceTotal,
+        ]);
     }
+
+
+    #[Route('/add-to-cart/{id}', name: 'add_to_cart')]
+    public function addToCart(Request $request, Article $article): JsonResponse
+    {
+        $cart = $request->getSession()->get('cart', []);
+
+        if (!isset($cart[$article->getId()])) {
+            $cart[$article->getId()] = [
+                'article' => $article,
+                'quantity' => 0,
+            ];
+        }
+
+        $cart[$article->getId()]['quantity']++;
+
+        $request->getSession()->set('cart', $cart);
+
+        $cartItemCount = $this->getCartItemCount($request);
+
+        return new JsonResponse([
+            'success' => true,
+            'cartItemCount' => $cartItemCount,
+        ]);
+    }
+
+    private function getCartItemCount(Request $request): int
+    {
+        $cart = $request->getSession()->get('cart', []);
+
+        $count = 0;
+
+        foreach ($cart as $cartItemData) {
+            $count += $cartItemData['quantity'];
+        }
+
+        return $count;
+    }
+
+
+
+
+    
 }
