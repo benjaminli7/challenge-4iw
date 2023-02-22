8b213579a6130cdca45aefd2cf4507cddb0e8301
syalicheff <yalicheff.sebastien@gmail.com>
2023-02-22 21:45:29 +0100

    SMS Feature Done

--- src/Service/SmsService.php
+++ src/Service/SmsService.php
@@ -17,22 +17,38 @@ class SmsService
     }
     public function sendSms(Order $order): void
     {
-
         $twilioNumber = getenv('TWILIO_PHONE_NUMBER'); // get Twilio number from .env file
-
         $recipientNumber = $order->getOrderPhoneNumber();
         // make it this format +33761598898 actual format is 0761598898
         $recipientNumber = '+33' . substr($recipientNumber, 1);
 
-        // Create a new Twilio client with your account SID and auth token
+        $status = $order->getStatus();
+
+        // Set the message based on the status
+        switch ($status) {
+            case 'ONGOING':
+                $messageBody = sprintf('Your order #%d is now being prepared.', $order->getId());
+                break;
+            case 'TO_PICK_UP':
+                $messageBody = sprintf('Your order #%d is ready for pick up!', $order->getId());
+                break;
+            case 'DONE':
+                $messageBody = sprintf('Your order #%d has been delivered. Enjoy your meal!', $order->getId());
+                break;
+            default:
+                $messageBody = sprintf('Your order #%d has been updated to %s.', $order->getId(), $status);
+                break;
+        }
+
         // Send the SMS message
         $message = $this->client->messages->create(
             $recipientNumber,
             [
                 'from' => $this->phoneNumber,
-                'body' => sprintf('Your order #%d has been updated to %s.', $order->getId(), $order->getStatus()),
+                'body' => $messageBody,
             ]
         );
     }
+
 
 }
