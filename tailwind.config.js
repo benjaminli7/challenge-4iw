3fa23df99bf262086548b1c3a16f0e287b4f837a
benjaminli7 <nicky01.bl@gmail.com>
2023-02-24 22:01:08 +0100

    started integration admin and client

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,34 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  theme: {
-    colors: {
-      'primary-red': '#76004F',
-      'primary-gray': '#F5F5F5',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      yellow: colors.yellow,
-      blue: colors.blue,
-      red: colors.red,
-      green: colors.green,
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+        colors: {
+            "primary-red": "#76004F",
+            "primary-gray": "#F5F5F5",
+            "front-red": "#CC0000",
+            "primary-pink": "#D24E8A",
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            yellow: colors.yellow,
+            blue: colors.blue,
+            red: colors.red,
+            green: colors.green
+        },
+        extend: {
+            backgroundImage: {
+                login: "url('/public/images/bg-login.png')"
+            }
+        },
+        fontFamily: {
+            raleway: ["Raleway", "sans-serif"],
+            ibm: ["IBM Plex Sans", "sans-serif"]
+        }
     },
-    extend: {
-      backgroundImage: {
-        'login': "url('/public/images/bg-login.png')",
-      },
-    },
-    fontFamily: {
-      'raleway': ['Raleway', 'sans-serif'],
-    }
-  },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
-}
+    plugins: [require("flowbite/plugin")]
+};
