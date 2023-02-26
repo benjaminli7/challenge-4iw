41ec8ef0bbf57317716c9b1c1ef47da3742e7fcd
benjaminli7 <nicky01.bl@gmail.com>
2023-02-26 16:05:08 +0100

    add integration tags

--- src/Form/TagType.php
+++ src/Form/TagType.php
@@ -6,13 +6,39 @@ use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 
 class TagType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez un tag',
+                    ]),
+                ],
+                'label' => 'Nom du tag',
+                'attr' => [
+                    'placeholder' => 'Nom du tag',
+                ],
+                'required' => true,
+            ])
+            ->add('color', ColorType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez une couleur',
+                    ]),
+                ],
+                'label' => 'Couleur du tag',
+                'attr' => [
+                    'placeholder' => 'Couleur du tag',
+                ],
+                'required' => true,
+            ])
         ;
     }
 
